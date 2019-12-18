02d3caf0cb939d78652a89f69a60eb3dfe0d73d4
Bryna Hazelton <bryna.hazelton@gmail.com>
2019-12-18 09:31:35 -0800

    flake8 fixes

--- pyradiosky/tests/__init__.py
+++ pyradiosky/tests/__init__.py
@@ -5,7 +5,7 @@ try:
     import astropy_healpix  # noqa
 
     healpix_installed = True
-except(ImportError):
+except (ImportError):
     healpix_installed = False
-reason = 'astropy_healpix is not installed, skipping tests that require it.'
+reason = "astropy_healpix is not installed, skipping tests that require it."
 skipIf_no_healpix = pytest.mark.skipif(not healpix_installed, reason=reason)
