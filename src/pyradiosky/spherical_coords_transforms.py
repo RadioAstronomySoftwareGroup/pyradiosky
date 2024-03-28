a6b4ef3025b79abf82f77cf9ceb3cb2f1a93743a
Bryna Hazelton <bryna.hazelton@gmail.com>
2024-03-28 13:55:42 -0700

    Fix several places where files listed the wrong license type

--- src/pyradiosky/spherical_coords_transforms.py
+++ src/pyradiosky/spherical_coords_transforms.py
@@ -1,6 +1,6 @@
 # -*- mode: python; coding: utf-8 -*
 # Copyright (c) 2019 Radio Astronomy Software Group
-# Licensed under the 3-clause BSD License
+# Licensed under the 2-clause BSD License
 """Methods for doing spherical coordinate transformations on vectors."""
 
 import numpy as np
